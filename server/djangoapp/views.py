7013b1c49e66fcf22060bcb8266a6285b307f365
lexlu726 <lexlu726@hotmail.com>
2024-01-02 01:03:52 -0500

    added auth process

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -36,14 +36,39 @@ def contact(request):
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
 # ...
+def login_request(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user  = authenticate(username=username, password=password)
+        if user != None:
+            login(request, user)
+            return redirect("djangoapp:index")
+        else:
+            return redirect("djangoapp:registration")
+
+
 
 # Create a `logout_request` view to handle sign out request
 # def logout_request(request):
 # ...
+def logout_request(request):
+    logout(request)
+    return redirect("djangoapp:index")
 
 # Create a `registration_request` view to handle sign up request
 # def registration_request(request):
 # ...
+def registration_request(request): 
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/registration.html', context) 
+    # if request.method == "POST":
+    #     username = request.POST['username']
+    #     password = request.POST['password']
+    #     user = User.objects.create_user(username=username, password=password)
+    #     login(request,user)
+    #     return redirect("djangoapp:registration")
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
