40233d7df58f70a26613b8164aaa3f4fc5f4b05a
lexlu726 <lexlu726@hotmail.com>
2024-01-03 03:47:52 -0500

    [feature] add carMake, carModel

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
 # from .models import related models
+from .models import CarMake, CarModel
 
 
-# Register your models here.
-
-# CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 2
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    fields = ["CARMAKE", "Name", "Dealer_Id", "Type", "Year"]
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    fields = ["Name", "Description"]
+
 
 # Register models here
+admin.site.register(CarModel, CarModelAdmin)
+admin.site.register(CarMake, CarMakeAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,7 +9,11 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
-
+class CarMake(models.Model):
+    Name = models.CharField(max_length = 30 )
+    Description = models.CharField(max_length = 30 )
+    def __str__(self):
+        return f'Name: {self.Name}, Description: {self.Description}'
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -19,7 +23,14 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
-
+class CarModel(models.Model):
+    Name = models.CharField(max_length = 30 )
+    Dealer_Id = models.IntegerField(default=1,primary_key=True)
+    Type = models.CharField(max_length = 30 )
+    Year = models.DateField()
+    CARMAKE = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    def __str__(self):
+        return f'Name: {self.Name}'
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
